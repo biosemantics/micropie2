540570009af9732c640e87521b90e4aa0f9eeeb3
elviscat <elviscat@gmial.com>
2014-02-13 16:49:05 -0700

    add n-folder cross validation class

---
+++ src/main/java/edu/arizona/biosemantics/micropie/classify/NFolderCrossValidation.java
@@ -0,0 +1,39 @@
+package edu.arizona.biosemantics.micropie.classify;
+
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.Utils;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+
+import java.util.List;
+import java.util.Random;
+
+import edu.arizona.biosemantics.micropie.model.Sentence;
+
+public class NFolderCrossValidation {
+
+	
+	protected Instances instances;
+
+	
+	public NFolderCrossValidation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+
+		
+		
+		
+		
+		
+	}
+
+}
